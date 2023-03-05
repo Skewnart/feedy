fef4bd1d168b1d37eed126a6bf527d053894c233
Skewnart <corentinz@outlook.fr>
2023-03-05 22:55:06 +0100

    Setup Firebase and link w/ Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,15 @@ import 'package:feedy/pages/old/account_page.dart';
 import 'package:feedy/pages/home.page.dart';
 import 'package:feedy/pages/login.page.dart';
 import 'package:feedy/pages/splash.page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await Supabase.initialize(
     url: 'INSERT URL HERE',
